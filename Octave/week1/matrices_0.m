d6f3bc4b2bc80d8d775012658b42997b14b482fe
ingyu1008 <ingyu1008@gmail.com>
2021-08-13 12:26:46 +0900

    ADD: week1 basic octave syntax aboutt matrices

---
+++ Octave/week1/matrices_0.m
@@ -0,0 +1,12 @@
+% Question 1
+Ans_1 = [8, 6, 9; 10, 1, 10] + [3, 10, 2; 6, 1, -1];
+
+% Question 2
+Ans_2 = 2 * [4, 5; 1, 7];
+
+% Question 3
+Ans_3 = [4; 6; 7] / 2 - 3 * [2; 1; 0];
+
+printf("Question %d:\n%s\n", 1, disp(Ans_1))
+printf("Question %d:\n%s\n", 2, disp(Ans_2))
+printf("Question %d:\n%s\n", 3, disp(Ans_3))
---
+++ Octave/week1/matrices_1.m
@@ -0,0 +1,8 @@
+% Question 1
+Ans_1 = size([1, 2, 1, 5; 0, 3, 0, 4; -1, -2, 0, 0] * [1; 3; 2; 1]);
+
+% Question 2
+Ans_2 = [1, 0, 3; 2, 1, 5; 3, 1, 2] * [1; 6; 2];
+
+printf("Question %d:\n%s\n", 1, disp(Ans_1))
+printf("Question %d:\n%s\n", 2, disp(Ans_2))
